f7e83edbf141715678ca7c71c5da965669a3d4cd
tligodsp <43437080+tligodsp@users.noreply.github.com>
2019-11-07 00:17:23 +0700

    Add welcome screen

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image } from 'react-native-elements';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.introduction}>
+        <Image
+          source={ require('./assets/communism_ftw.png') }
+          style={{ width: 200, height: 200 }}
+          containerStyle={{ overflow: "hidden", borderRadius: 10 }}
+        />
+        <Text style={styles.introductionText}>Learn English for free.</Text>
+        <Text style={styles.introductionText}>Forever.</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button 
+          title="Get Started"
+          titleStyle={styles.buttonText}
+          buttonStyle={styles.button}
+        />
+        <Button 
+          title="I already have an account"
+          titleStyle={styles.buttonText}
+          buttonStyle={styles.button}
+          type="outline"
+        />
+      </View>
     </View>
   );
 }
@@ -16,4 +38,28 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  introduction: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // backgroundColor: "red"
+  },
+  introductionText: {
+    fontSize: 22,
+    marginTop: 10
+  },
+  buttonContainer: {
+    marginBottom: 30,
+    textTransform: "uppercase"
+  },
+  button: {
+    height: 50,
+    width: "100%",
+    marginVertical: 5,
+    borderRadius: 10,
+    borderWidth: 2
+  },
+  buttonText: {
+    textTransform: "uppercase"
+  }
 });
